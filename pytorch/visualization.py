ff9e4db86cb1dd852281281f7f9b531f2eed2291
idealwei <wei.zhou.official@gmail.com>
2018-05-05 11:51:51 +0800

    vscod

---
+++ pytorch/visualization.py
@@ -0,0 +1,14 @@
+import visdom
+
+# create an environment
+vis = visdom.Visdom(env='env_name')
+
+# text
+vis.text('text', win='win_name')
+# append
+vis.text('append content', win='win_name', append=True)
+
+
+# line example
+for i in range(1000):
+    vis.line(X=torch.FloatTensor([i]), Y=torch.FloatTensor([i**2]), win='loss', update='append' if i > 0 else None)
